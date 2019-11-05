9c916593ad7eab82f9d9015f5b63b9c807b6781d
Garin ichsan <42213809+94gin@users.noreply.github.com>
2019-11-05 11:36:20 +0700

    add the main header and body

---
+++ array.c
@@ -0,0 +1,96 @@
+/* Nama : Garin Ichsan Nugraha */
+/* NIM : 18218050 */
+/* Tanggal : 10 September 2019 */
+/* Topik Praktikum : array.c */
+/* Deskripsi : implementasi dari array.h */
+
+#include <stdio.h>
+#include "array.h"
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create tabel kosong */
+void MakeEmpty (TabInt *T){
+    (*T).Neff=0;
+}
+/* I.S. sembarang */
+/* F.S. Terbentuk tabel T kosong dengan kapasitas IdxMax-IdxMin+1 */
+
+/* ********** SELEKTOR ********** */
+/* *** Banyaknya elemen *** */
+int NbElmt (TabInt T){
+    return T.Neff;
+}
+/* Mengirimkan banyaknya elemen efektif tabel */
+/* Mengirimkan nol jika tabel kosong */
+/* *** Daya tampung container *** */
+int MaxNbEl (TabInt T){
+    return IdxMax;
+}
+/* Mengirimkan maksimum elemen yang dapat ditampung oleh tabel */
+/* *** Selektor INDEKS *** */
+IdxType GetFirstIdx (TabInt T){
+    return IdxMin;
+}
+/* Prekondisi : Tabel T tidak kosong */
+/* Mengirimkan indeks elemen pertama */
+IdxType GetLastIdx (TabInt T){
+    return T.Neff;
+}
+/* Prekondisi : Tabel T tidak kosong */
+/* Mengirimkan indeks elemen terakhir */
+/* *** Menghasilkan sebuah elemen *** */
+ElType GetElmt (TabInt T, IdxType i){
+    return T.TI[i];
+}
+/* Prekondisi : Tabel tidak kosong, i antara FirstIdx(T)..LastIdx(T) */
+/* Mengirimkan elemen tabel yang ke-i */
+
+/* *** Selektor SET : Mengubah nilai TABEL dan elemen tabel *** */
+/* Untuk type private/limited private pada bahasa tertentu */
+void SetTab (TabInt Tin, TabInt *Tout){
+    (*Tout).Neff=Tin.Neff;
+    for(int i=IdxMin;i<=Tin.Neff;i++){
+        (*Tout).TI[i]=Tin.TI[i];
+    }
+}
+/* I.S. Tin terdefinisi, sembarang */
+/* F.S. Tout berisi salinan Tin */
+/* Assignment THsl -> Tin */
+void SetEl (TabInt *T, IdxType i, ElType v){
+    (*T).TI[i]=v;
+}
+/* I.S. T terdefinisi, sembarang */
+/* F.S. Elemen T yang ke-i bernilai v */
+/* Mengeset nilai elemen tabel yang ke-i sehingga bernilai v */
+void SetNeff (TabInt *T, IdxType N){
+    (*T).Neff=N;
+}
+/* I.S. T terdefinisi, sembarang */
+/* F.S. Nilai indeks efektif T bernilai N */
+/* Mengeset nilai indeks elemen efektif sehingga bernilai N */
+
+/* ********** Test Indeks yang valid ********** */
+boolean IsIdxValid (TabInt T, IdxType i){
+    return ((i>=IdxMin)&&(IdxMax>=i));
+}
+/* Prekondisi : i sembarang */
+/* Mengirimkan true jika i adalah indeks yang valid utk ukuran tabel */
+/* yaitu antara indeks yang terdefinisi utk container*/
+boolean IsIdxEff (TabInt T, IdxType i){
+    return ((i>=IdxMin)&&(T.Neff>=i));
+}
+/* Prekondisi : i sembarang*/
+/* Mengirimkan true jika i adalah indeks yang terdefinisi utk tabel */
+/* yaitu antara FirstIdx(T)..LastIdx(T) */
+
+/* ********** TEST KOSONG/PENUH ********** */
+/* *** Test tabel kosong *** */
+boolean IsEmpty (TabInt T){
+    return (T.Neff==0);
+}
+/* Mengirimkan true jika tabel T kosong, mengirimkan false jika tidak */
+/* *** Test tabel penuh *** */
+boolean IsFull (TabInt T){
+    return (T.Neff==IdxMax);
+}
+/* Mengirimkan true jika tabel T penuh, mengirimkan false jika tidak */
