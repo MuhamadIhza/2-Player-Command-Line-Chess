9c389d7ee4d09b645651c1581d79d0d30c01f736
94gin <garin.kra@gmail.com>
2019-11-06 15:13:49 +0700

    Penataan dan inisialisasi

---
+++ main.c
@@ -0,0 +1,15 @@
+// Program Utama Catur
+
+// Library yang digunakan
+#include <stdio.h>
+#include "ADT/array.h"
+
+// Kamus Global
+int i;
+
+// Algoritma
+int main(){
+    while(1){
+        
+    }
+}
