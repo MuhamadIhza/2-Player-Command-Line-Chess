cabc732aabb37fb923030e7f75ff2565ceec7472
Muhamad Ihza <jambeuk@gmail.com>
2019-11-23 16:27:25 +0700

    inisiasi giliran

--- trial/prog/giliran.c
+++ trial/prog/giliran.c
@@ -1,4 +1,18 @@
 #include "../def/queue.h"
 #include <stdio.h>
 
-
+void Urutan(Queue *Q1){
+    char a;
+    char b;
+    int i;
+    a = 'Player 1';
+    b = 'Player 2';
+    CreateEmpty(Q1,50);
+    for (i = 1;i < MaxEl(*Q1);i++){
+        if (i % 2 == 1){
+            Add(Q1,a);
+        } else{
+            Add(Q1,b);
+        }
+    }
+}
